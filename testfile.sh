7bcb84b22da00877f6b5960e33b4256f15a2830c
sub0ne1 <iamsubhajit.d@gmail.com>
2024-06-06 22:24:16 -0400

    changed output dir and exec sequence

--- testfile.sh
+++ testfile.sh
@@ -1,24 +1,25 @@
-#!/bin/bash 
+#!/bin/bash
 
-while getopts f: flag
-    do 
+while getopts f: flag; do
 	case "${flag}" in
-	f) filename=${OPTARG};;
+	f) filename=${OPTARG} ;;
 	esac
-    done
+done
 
-OUT_FILE=$(echo $filename | cut -d '.' -f 1)
-ERR=0;
-echo [`date`] ": running: $filename"
-g++ $filename -o out/$OUT_FILE || ERR=1;
-
-if [ $ERR -eq 0 ]
-    then
-	echo [`date`] ": output:"
-	echo
-	./out/$OUT_FILE
-	echo
-    else
-	echo [`date`] ": could not compile. check for errors"
+OUT_FILE=$(echo $(basename ${filename}) | cut -d '.' -f 1)
+OUT_DIR=".out"
+ERR=0
+echo [$(date)] ": running: $filename"
+if [ ! -d "$OUT_DIR" ]; then
+	mkdir "$OUT_DIR"
 fi
 
+g++ $filename -o .out/$OUT_FILE || ERR=1
+
+if [ $ERR -eq 0 ]; then
+	echo [$(date)] ": output:"
+	./$OUT_DIR/$OUT_FILE
+	echo
+else
+	echo [$(date)] ": could not compile. check for errors"
+fi
